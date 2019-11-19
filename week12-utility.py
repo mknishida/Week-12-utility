cc03e99b7d0fe34c8bbf41e92ebff85cefe284ac
Morgan Nishida <mknishida@mymail.mines.edu>
2019-11-18 23:01:51 -0700

    UpdatedString

--- week12-utility.py
+++ week12-utility.py
@@ -10,3 +10,6 @@ def LoadFile(file):
     f = open(file,"r")
     lines = f.readlines()
     print("OUTPUT" ,lines)
+
+def UpdateString(string_1, string_2, index):
+    print(string_1[:index] + string_2 + string_1[index+1:])
